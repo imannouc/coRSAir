06fc75f2d88604647f8e6413318a33b2eced6b09
Ismail Mannoucha <ismannou@c2r10s2.42malaga.com>
2023-05-23 19:32:07 +0200

    corsair done , i think

--- corsair.c
+++ corsair.c
@@ -10,6 +10,7 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 typedef struct s_rsa
 {
@@ -54,13 +55,42 @@ void store_n_e(char *keyfile, t_rsa *trsa)
     // Extract the modulus and exponent from the RSA key
     trsa->n = BN_dup(RSA_get0_n(rsa));
     trsa->e = BN_dup(RSA_get0_e(rsa));
+    printf("\33[32mmodulus :\33[0m\n");
+    BN_print_fp(stdout,trsa->n);
+    printf("\n");
+    printf("\33[32mexponent :\33[0m\n");
+    BN_print_fp(stdout,trsa->e);
+    printf("\n");
+
     // https://www.openssl.org/docs/man1.0.2/man3/bn.html
     //  int BN_print_fp(FILE *fp, const BIGNUM *a);
     RSA_free(rsa);
 }
 
+
+/*
+    https://manpages.debian.org/stretch/libssl-doc/RSA_set0_key.3ssl.en.html
+    int RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d);
+    int RSA_set0_factors(RSA *r, BIGNUM *p, BIGNUM *q);
+*/
+
+RSA *generate_private_key(BIGNUM *e, BIGNUM *n, BIGNUM *p , BIGNUM *q, BIGNUM *d)
+{
+    RSA * privateKey = RSA_new();
+
+    RSA_set0_key(privateKey,n,e,d);
+    RSA_set0_factors(privateKey,p,q);
+    return privateKey;
+}
+
+void    ft_leaks()
+{
+    system("leaks -q a.out");
+}
+
 int main(int ac, char **av)
 {
+    atexit(ft_leaks);
     if (ac != 5) {
         printf("Usage : ./corsair <pubCert1>.pem <pubCert2>.pem <msg1>.bin <msg2.bin>");
         return 1;
@@ -68,8 +98,6 @@ int main(int ac, char **av)
     t_rsa *key1;
     t_rsa *key2;
     BIGNUM *commonPrime = BN_new();
-    BIGNUM *mod_inv1 = BN_new();
-    BIGNUM *mod_inv2 = BN_new();
     BIGNUM *one = BN_new();
     BIGNUM *phi1 = BN_new();
     BIGNUM *phi2 = BN_new();
@@ -79,7 +107,6 @@ int main(int ac, char **av)
         printf("BN_dec2bn failed to convert 1 to BN");
         exit(1);
     }
-    BN_print_fp(stdout,one);
     key1 = malloc(sizeof(t_rsa));
     key2 = malloc(sizeof(t_rsa));
     // For the 'q' I use commonPrime for both, so no need to allocate, and for the 'n' and 'e' they get allocated by BN_dup(); inside store_n_e(); 
@@ -95,12 +122,6 @@ int main(int ac, char **av)
     store_n_e(av[1],key1);
     store_n_e(av[2],key2);
 
-    printf("\33[32mmodulus :\33[0m\n");
-    BN_print_fp(stdout,key1->n);
-    printf("\n");
-    printf("\33[32mexponent :\33[0m\n");
-    BN_print_fp(stdout,key1->e);
-    printf("\n");
 
     // https://www.openssl.org/docs/man3.0/man3/BN_gcd.html
     // int BN_gcd(BIGNUM *r, BIGNUM *a, BIGNUM *b, BN_CTX *ctx); // calculates gcd(a,b), places the result in r
@@ -138,21 +159,57 @@ int main(int ac, char **av)
     // https://www.openssl.org/docs/man3.1/man3/BN_mod_inverse.html
     // BIGNUM *BN_mod_inverse(BIGNUM *r, BIGNUM *a, const BIGNUM *n, BN_CTX *ctx);
 
-    BN_mod_inverse(mod_inv1,key1->e,phi1,tmp);
-    BN_mod_inverse(mod_inv2,key2->e,phi2,tmp);
-    BN_print_fp(stdout,mod_inv1);
+    BN_mod_inverse(key1->d,key1->e,phi1,tmp);
+    BN_mod_inverse(key2->d,key2->e,phi2,tmp);
+    BN_print_fp(stdout,key1->d);
     printf("\n");
-    BN_print_fp(stdout,mod_inv2);
+    BN_print_fp(stdout,key2->d);
 
 
+    BIGNUM *c2 = BN_dup(commonPrime);
+    RSA * privateKey1 = generate_private_key(key1->e,key1->n,key1->p,commonPrime,key1->d);
+    RSA * privateKey2 = generate_private_key(key2->e,key2->n,key2->p,c2,key2->d);
+    char to[1024];
+    char from[1024];
 
-    // int PEM_write_bio_PrivateKey(BIO *bp, const EVP_PKEY *x, const EVP_CIPHER *enc,
-        // unsigned char *kstr, int klen, pem_password_cb *cb, void *u);
-    
+    int fd = open(av[3],O_RDONLY);
+    if (fd < 0){
+        printf("Failed to open msg file.");
+        exit(1);
+    }
+    int len1 = read(fd,from,1024);
+    // int RSA_private_decrypt(int flen, unsigned char *from, unsigned char *to, RSA *rsa, int padding);
+    RSA_private_decrypt(len1,from,to,privateKey1,RSA_PKCS1_PADDING);
+    printf("\n\33[3mDecrypted msg :\33[0m %s",to);
+
+    fd = open(av[4],O_RDONLY);
+    if (fd < 0){
+        printf("Failed to open msg file.");
+        exit(1);
+    }
+    len1 = read(fd,from,1024);
+
+    RSA_private_decrypt(len1,from,to,privateKey2,RSA_PKCS1_PADDING);
+    printf("\n\33[3mDecrypted msg :\33[0m %s",to);
 
     // Clean up
     free(key1);
     free(key2);
-    // RSA_free(rsa);
+    RSA_free(privateKey1);
+    RSA_free(privateKey2);
+
+    BN_free(key1->pMinusOne);
+    BN_free(key1->qMinusOne);
+    // BN_free(key1->d);
+    BN_free(phi1);
+
+
+    BN_free(key2->pMinusOne);
+    BN_free(key2->qMinusOne);
+    // BN_free(key2->d);
+    BN_free(phi2);
+
+    BN_CTX_free(tmp);
+    BN_free(one);
     return 0;
 }
