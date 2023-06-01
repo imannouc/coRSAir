887e2e51461aa5c3cf2ed55638ea6fcbaef32fc6
Ismail Mannoucha <ismannou@c2r10s2.42malaga.com>
2023-06-01 18:21:37 +0200

    replaced my own struct with RSA struct , clean up.

--- corsair.c
+++ corsair.c
@@ -12,69 +12,43 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-typedef struct s_rsa
-{
-    BIGNUM *n; // modulus
-    BIGNUM *e; // exponent
-    BIGNUM *p; // first prime
-    BIGNUM *q; // second prime
-    BIGNUM *d; // private
-    BIGNUM *pMinusOne; // p - 1
-    BIGNUM *qMinusOne; // q - 1
-} t_rsa;
+// void    ft_leaks()
+// {
+//     system("leaks -q a.out");
+// }
 
-void store_n_e(char *keyfile, t_rsa *trsa)
+void store_n_e(char *keyfile, BIGNUM *n, BIGNUM *e)
 {
     RSA* rsa = NULL;
     // Create a BIO object for file I/O
     printf("\33[34mAccessing file : \33[33m%s\33[0m\n",keyfile);
     BIO* bio = BIO_new_file(keyfile, "r");
     if (bio == NULL) {
-        printf("Failed to open the key file.\n");
+        printf("\n\33[31mFailed to open the key file.\n\33[0m");
         exit(1);
     }
 
     // Load the public key from the file
     rsa = PEM_read_bio_RSA_PUBKEY(bio, &rsa, NULL, NULL);
-    // https://www.openssl.org/docs/man3.1/man3/PEM_read_bio_RSA_PUBKEY.html
     BIO_free(bio);
-
     if (rsa == NULL) {
-        printf("Failed to load the public key.\n");
+        printf("\n\33[31mFailed to load the public key.\n\33[0m");
         exit(1);
     }
-        // {
-        //     FILE *out = stdout;
-        //     RSA_print_fp(out, rsa, 0);
-        // }
-    // https://www.openssl.org/docs/man1.1.1/man3/RSA_get0_key.html
-    // const BIGNUM *RSA_get0_n(const RSA *d);
-    // const BIGNUM *RSA_get0_e(const RSA *d);
 
-    // https://linux.die.net/man/3/bn_dup
-    // Extract the modulus and exponent from the RSA key
-    trsa->n = BN_dup(RSA_get0_n(rsa));
-    trsa->e = BN_dup(RSA_get0_e(rsa));
+    BN_copy(n,RSA_get0_n(rsa));
+    BN_copy(e,RSA_get0_e(rsa));
     printf("\33[32mmodulus :\33[0m\n");
-    BN_print_fp(stdout,trsa->n);
+    BN_print_fp(stdout,n);
     printf("\n");
     printf("\33[32mexponent :\33[0m\n");
-    BN_print_fp(stdout,trsa->e);
+    BN_print_fp(stdout,e);
     printf("\n");
 
-    // https://www.openssl.org/docs/man1.0.2/man3/bn.html
-    //  int BN_print_fp(FILE *fp, const BIGNUM *a);
     RSA_free(rsa);
 }
 
-
-/*
-    https://manpages.debian.org/stretch/libssl-doc/RSA_set0_key.3ssl.en.html
-    int RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d);
-    int RSA_set0_factors(RSA *r, BIGNUM *p, BIGNUM *q);
-*/
-
-RSA *generate_private_key(BIGNUM *e, BIGNUM *n, BIGNUM *p , BIGNUM *q, BIGNUM *d)
+RSA *construct_key(BIGNUM *e, BIGNUM *n, BIGNUM *p , BIGNUM *q, BIGNUM *d)
 {
     RSA * privateKey = RSA_new();
 
@@ -83,20 +57,50 @@ RSA *generate_private_key(BIGNUM *e, BIGNUM *n, BIGNUM *p , BIGNUM *q, BIGNUM *d
     return privateKey;
 }
 
-void    ft_leaks()
-{
-    system("leaks -q a.out");
+
+void    decrypt_file(char *filename, RSA *privateKey){
+    char to[1024];
+    char from[1024];
+
+    int fd = open(filename,O_RDONLY);
+    printf("\n\33[34mAccessing msg file : \33[33m%s\33[0m",filename);
+    if (fd < 0){
+        printf("\n\33[31mFailed to open msg file.\33[0m");
+        exit(1);
+    }
+    int len1 = read(fd,from,1024);
+    close(fd);
+    if (RSA_private_decrypt(len1,from,to,privateKey,RSA_PKCS1_PADDING) == -1)
+    {
+        printf("\n\33[31mFailed to Decrypt the msg file.\n\33[0m");
+        exit(1);
+    }
+    printf("\n\33[32mDecrypted msg :\33[0m %s",to);
 }
 
 int main(int ac, char **av)
 {
-    atexit(ft_leaks);
+    // atexit(ft_leaks);
     if (ac != 5) {
         printf("Usage : ./corsair <pubCert1>.pem <pubCert2>.pem <msg1>.bin <msg2.bin>");
         return 1;
     }
-    t_rsa *key1;
-    t_rsa *key2;
+    /* First RSA key */
+    BIGNUM *p1 = BN_new();
+    BIGNUM *n1 = BN_new();
+    BIGNUM *e1 = BN_new();
+    BIGNUM *d1 = BN_new();
+    BIGNUM *pMinusOne1 = BN_new();
+    BIGNUM *qMinusOne1 = BN_new();
+
+    /* Second RSA key */
+    BIGNUM *p2 = BN_new();
+    BIGNUM *n2 = BN_new();
+    BIGNUM *e2 = BN_new();
+    BIGNUM *d2 = BN_new();
+    BIGNUM *pMinusOne2 = BN_new();
+    BIGNUM *qMinusOne2 = BN_new();
+
     BIGNUM *commonPrime = BN_new();
     BIGNUM *one = BN_new();
     BIGNUM *phi1 = BN_new();
@@ -107,134 +111,96 @@ int main(int ac, char **av)
         printf("BN_dec2bn failed to convert 1 to BN");
         exit(1);
     }
-    key1 = malloc(sizeof(t_rsa));
-    key2 = malloc(sizeof(t_rsa));
-    // For the 'q' I use commonPrime for both, so no need to allocate, and for the 'n' and 'e' they get allocated by BN_dup(); inside store_n_e(); 
-    key1->p = BN_new();
-    key1->d = BN_new();
-    key1->pMinusOne = BN_new();
-    key1->qMinusOne = BN_new();
-
-    key2->p = BN_new();
-    key2->d = BN_new();
-    key2->pMinusOne = BN_new();
-    key2->qMinusOne = BN_new();
-    store_n_e(av[1],key1);
-    store_n_e(av[2],key2);
-
-    // https://www.openssl.org/docs/man3.0/man3/BN_gcd.html
-    // int BN_gcd(BIGNUM *r, BIGNUM *a, BIGNUM *b, BN_CTX *ctx); // calculates gcd(a,b), places the result in r
-    // the fourth argument is a temporary struct used by BN to avoid repeated expense mem allocs
-    // BN_CTX *BN_CTX_new(void);
+    store_n_e(av[1],n1,e1);
+    store_n_e(av[2],n2,e2);
+    printf("\33[96m----------------------------------------------------\33[0m");
     BN_CTX *tmp = BN_CTX_new();
 
-    BN_gcd(commonPrime,key1->n,key2->n,tmp);
+    BN_gcd(commonPrime,n1,n2,tmp);
     
-    // https://www.openssl.org/docs/man3.0/man3/BN_cmp.html
-    // int BN_cmp(const BIGNUM *a, const BIGNUM *b);
-    if (BN_cmp(commonPrime,one) == 1)
-    {
+    if (BN_cmp(commonPrime,one) == 1) {
         printf("\33[92m\nPrime in common :\33[0m\n");
         BN_print_fp(stdout,commonPrime);
     }
-    else
-    {
-        printf("No primes in common, my job here is done, Bye ! :)\n");
-
-        BN_free(key1->p);
-        BN_free(key1->q);
-        BN_free(key1->e);
-        BN_free(key1->n);
-        BN_free(key1->pMinusOne);
-        BN_free(key1->qMinusOne);
-        BN_free(key1->d);
-        BN_free(phi1);
-
-
-        BN_free(key2->p);
-        BN_free(key2->q);
-        BN_free(key2->e);
-        BN_free(key2->n);
-        BN_free(key2->pMinusOne);
-        BN_free(key2->qMinusOne);
-        BN_free(key2->d);
-        BN_free(phi2);
-
-        BN_CTX_free(tmp);
-        BN_free(one);
-        BN_free(commonPrime);
-        free(key1);
-        free(key2);
-        exit(1);
+    else {
+        printf("\n\33[31mNo primes in common, my job here is done, Bye ! :)\33[0m\n");
+        exit(0);
     }
-    // int BN_div(BIGNUM *dv, BIGNUM *rem, const BIGNUM *a, const BIGNUM *d, BN_CTX *ctx);
-    BN_div(key1->p,NULL,key1->n,commonPrime,tmp);
-    BN_div(key2->p,NULL,key2->n,commonPrime,tmp);
+    BN_div(p1,NULL,n1,commonPrime,tmp);
+    BN_div(p2,NULL,n2,commonPrime,tmp);
 
-    // int BN_sub(BIGNUM *r, const BIGNUM *a, const BIGNUM *b);
-    BN_sub(key1->pMinusOne,key1->p,one);
-    BN_sub(key1->qMinusOne,commonPrime,one);
+    BN_sub(pMinusOne1,p1,one);
+    BN_sub(qMinusOne1,commonPrime,one);
 
-    BN_sub(key2->pMinusOne,key2->p,one);
-    BN_sub(key2->qMinusOne,commonPrime,one);
+    BN_sub(pMinusOne2,p2,one);
+    BN_sub(qMinusOne2,commonPrime,one);
 
-    // int BN_mul(BIGNUM *r, BIGNUM *a, BIGNUM *b, BN_CTX *ctx);
-    BN_mul(phi1,key1->pMinusOne,key1->qMinusOne,tmp);
-    BN_mul(phi2,key2->pMinusOne,key2->qMinusOne,tmp);
-
-    // https://www.openssl.org/docs/man3.1/man3/BN_mod_inverse.html
-    // BIGNUM *BN_mod_inverse(BIGNUM *r, BIGNUM *a, const BIGNUM *n, BN_CTX *ctx);
-
-    BN_mod_inverse(key1->d,key1->e,phi1,tmp);
-    BN_mod_inverse(key2->d,key2->e,phi2,tmp);
-    // BN_print_fp(stdout,key1->d);
-    // printf("\n");
-    // BN_print_fp(stdout,key2->d);
+    BN_mul(phi1,pMinusOne1,qMinusOne1,tmp);
+    BN_mul(phi2,pMinusOne2,qMinusOne2,tmp);
 
 
-    BIGNUM *c2 = BN_dup(commonPrime);
-    RSA * privateKey1 = generate_private_key(key1->e,key1->n,key1->p,commonPrime,key1->d);
-    RSA * privateKey2 = generate_private_key(key2->e,key2->n,key2->p,c2,key2->d);
-    char to[1024];
-    char from[1024];
+    BN_mod_inverse(d1,e1,phi1,tmp);
+    BN_mod_inverse(d2,e2,phi2,tmp);
 
-    int fd = open(av[3],O_RDONLY);
-    if (fd < 0){
-        printf("Failed to open msg file.");
-        exit(1);
-    }
-    int len1 = read(fd,from,1024);
-    close(fd);
-    // int RSA_private_decrypt(int flen, unsigned char *from, unsigned char *to, RSA *rsa, int padding);
-    RSA_private_decrypt(len1,from,to,privateKey1,RSA_PKCS1_PADDING);
-    printf("\n\33[94mDecrypted msg :\33[0m %s",to);
+    printf("\33[92m\nPrivate factor d1 :\33[0m\n");
+    BN_print_fp(stdout,d1);
+    printf("\33[92m\nPrivate factor d2 :\33[0m\n");
+    BN_print_fp(stdout,d2);
 
-    fd = open(av[4],O_RDONLY);
-    if (fd < 0){
-        printf("Failed to open msg file.");
-        exit(1);
-    }
-    len1 = read(fd,from,1024);
-    close(fd);
-    RSA_private_decrypt(len1,from,to,privateKey2,RSA_PKCS1_PADDING);
-    printf("\33[94mDecrypted msg :\33[0m %s",to);
+    BIGNUM *commonPrime2 = BN_dup(commonPrime);
+    RSA * privateKey1 = construct_key(e1,n1,p1,commonPrime,d1);
+    RSA * privateKey2 = construct_key(e2,n2,p2,commonPrime2,d2);
+
+    decrypt_file(av[3],privateKey1);
+    decrypt_file(av[4],privateKey2);
 
     // Clean up
     RSA_free(privateKey1);
     RSA_free(privateKey2);
 
-    BN_free(key1->pMinusOne);
-    BN_free(key1->qMinusOne);
+    BN_free(pMinusOne1);
+    BN_free(qMinusOne1);
     BN_free(phi1);
 
-
-    BN_free(key2->pMinusOne);
-    BN_free(key2->qMinusOne);
+    BN_free(pMinusOne2);
+    BN_free(qMinusOne2);
     BN_free(phi2);
 
     BN_CTX_free(tmp);
     BN_free(one);
-    free(key1);
-    free(key2);
     return 0;
 }
+
+/*
+
+
+    // https://www.openssl.org/docs/man3.1/man3/PEM_read_bio_RSA_PUBKEY.html
+
+    https://www.openssl.org/docs/man1.1.1/man3/RSA_get0_key.html
+    const BIGNUM *RSA_get0_n(const RSA *d);
+    const BIGNUM *RSA_get0_e(const RSA *d);
+
+    https://www.openssl.org/docs/man3.0/man3/BN_gcd.html
+    int BN_gcd(BIGNUM *r, BIGNUM *a, BIGNUM *b, BN_CTX *ctx); // calculates gcd(a,b), places the result in r
+    the fourth argument is a temporary struct used by BN to avoid repeated expense mem allocs
+    BN_CTX *BN_CTX_new(void);
+
+    https://www.openssl.org/docs/man3.0/man3/BN_cmp.html
+    int BN_cmp(const BIGNUM *a, const BIGNUM *b);
+
+    int BN_div(BIGNUM *dv, BIGNUM *rem, const BIGNUM *a, const BIGNUM *d, BN_CTX *ctx);
+
+    int BN_sub(BIGNUM *r, const BIGNUM *a, const BIGNUM *b);
+
+    int BN_mul(BIGNUM *r, BIGNUM *a, BIGNUM *b, BN_CTX *ctx);
+
+    https://www.openssl.org/docs/man3.1/man3/BN_mod_inverse.html
+    BIGNUM *BN_mod_inverse(BIGNUM *r, BIGNUM *a, const BIGNUM *n, BN_CTX *ctx);
+
+    https://manpages.debian.org/stretch/libssl-doc/RSA_set0_key.3ssl.en.html
+    int RSA_set0_key(RSA *r, BIGNUM *n, BIGNUM *e, BIGNUM *d);
+    int RSA_set0_factors(RSA *r, BIGNUM *p, BIGNUM *q);
+
+    int RSA_private_decrypt(int flen, unsigned char *from, unsigned char *to, RSA *rsa, int padding);
+
+*/
